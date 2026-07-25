2899a3b567a75fcd31a5f3b50c91703915b95ac6
Danareynolds-coding <dana.reynolds@mscoding.org>
2026-07-24 20:30:35 -0400

    change

--- species.js
+++ species.js
@@ -7,16 +7,16 @@ fetch("https://swapi.dev/api/species/")
   data = response.json()
   return data
 })
-.then(data => data.forEach(data => {
-  const arrayObject = JSON.parse(data.data)
-  arrayObject.forEach(file => {
+.then(data => {
+  const parsedData =JSON.parse(data)
+  data.forEach(file => {
   const element =
   `<h2>Name: ${data.name}</h2>
    <p>Class: ${data.classification}</p>
    <p>Homeworld:${data.language}</p>
    <p>Charaters:${data.people}</p>`
   document.querySelector('div').insertAdjacentHTML('beforeend', element)
+  })
 })
-}))
 .catch(error => console.log(error))
 
