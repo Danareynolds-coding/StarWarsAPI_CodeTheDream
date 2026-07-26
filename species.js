15d4e7b30f39d27bbd0ddce3c75b7fbd92655ef7
Danareynolds-coding <dana.reynolds@mscoding.org>
2026-07-25 21:07:53 -0400

    worked o species and starships

--- species.js
+++ species.js
@@ -1,20 +1,31 @@
-
-fetch("https://swapi.dev/api/species/")
-    .then(response => {
-         if (!response.ok) {
-         throw new Error('Network response was not ok');
-         }
-    return response.json();
-    })
-  
-    .then(data => {
-        data.results.forEach(species => {
-            const markup = `<li><h3>Name:${species.name}</h3><p>Homeworld:${species.homeworld}</p><p>Designation:${species.designation}</p><p>Language: ${species.language}</p><p>Classification: ${species.classification}</p></li> ` ;
-            document.getElementById('species').
-            insertAdjacentHTML('beforeend', markup);
-            }
-            });
-        })
-    .catch (error => console.error('Error:', error));
+const container = document.getElementById('species');
+async function fetchSpecies() {
+  const url = ("https://swapi.dev/api/species/")
+  try{
+    const response = await fetch(url);
+    if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+    const data = await response.json();
+    container.innerHTML ='';
     
+    data.results.forEach(species => {
+      const speciesDiv = document.createElement('div');
+      speciesDiv.className = "species-card";
+      if (!null){
+      speciesDiv.innerHTML = `<div><h3>Name:${species.name}</h3><p>Homeworld:${species.homeworld}</p><p>Designation:${species.designation}</p><p>Language: ${species.language}</p><p>Classification: ${species.classification}</p></div> ` 
+      container.appendChild(speciesDiv);
+    };
+  });
+  } catch (error) {
+      console.error("Failed to fetch starships:", error.message);
+      container.innerHTML = `"<p style="color:red;">Failed to Load</p>`;
+  }
+}
+ fetchSpecies();
 
+
+
+
+
+          
--- starships.js
+++ starships.js
@@ -1,20 +1,29 @@
-const starshipsUrl = "https://swapi.dev/api/starships/"
-
-async function starshipsData() {
+const container = document.getElementById('starships');
+async function fetchStarships() {
+  const url = "https://swapi.dev/api/starships/"
   try{
-    const response = await fetch(starshipsUrl);
+    const response = await fetch(url);
     if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
   }
     const data = await response.json();
-    .then(data => {
-      data.results.forEach(item => {
-      `${item.name}-Model:${item.model}-Pilots:${item.pilots.length}`
+    container.innerHTML ='';
+    data.results.forEach(starships => {
+      const starshipsDiv = document.createElement('div');
+      starshipsDiv.className = "starships-card";
+      starshipsDiv.innerHTML = `<h2>${starships.name}</h2> <p>Model:${starships.model}</p> <p>Pilots:${starships.pilots}</p>`;
+      container.appendChild(starshipsDiv)
     });
-    }) 
+  } catch (error) {
+      console.error("Failed to fetch starships:", error.message);
+      container.innerHTML = `"<p style="color:red;">Failed to Load</p>`;
+  }
+}
+ fetchStarships();
+  
+
+
+
+
     
-}catch (error){
-  console.error("failed", error)
-}
-}
-starshipsData();
+
